6a67a6582f7092fbefd7ee815ca6ba427e016f15
Mario Ledesma <mledesma313@gmail.com>
2024-10-07 22:42:27 -0400

    react form complete

---
+++ src/Components/Authenticate.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export const Authenticate = ({ token, setToken }) => {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      if (result.success) {
+        setSuccessMessage(result.message);
+        setError(null);
+      } else {
+        setSuccessMessage(null);
+        setError("Please provide a valid token!");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  return (
+    <>
+      <h2>Authenticate!</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </>
+  );
+};
+
+export default Authenticate;
